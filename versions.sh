e18694647b2fa92b4caf794dec0882f2cd80ae9b
Karl Stoney <me@karlstoney.com>
2017-09-23 02:32:40 +0100

    Updated terraform and kubectl

--- versions.sh
+++ versions.sh
@@ -7,7 +7,7 @@ function versions() {
   echo " - ruby:      $(ruby --version | awk '{print $2}')"
   echo " - gcloud:    $(gcloud --version | head -n 1 | awk '{print $4}')"
   echo " - kubectl:   $(kubectl version | head -n 1 | awk -F "\"" '{print $6}')"
-  echo " - terraform: $(terraform --version | awk '{print $2}')"
+  echo " - terraform: $(terraform --version | head -n 1 | awk '{print $2}')"
   echo " - ansible:   $(ansible --version | head -n 1 | awk '{print $2}')"
 }
 versions
